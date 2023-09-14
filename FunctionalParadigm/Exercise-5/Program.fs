a8116e261c36ea648bad54b5603aed42d3404868
Johanx22x <johanrodsa2210@gmail.com>
2023-09-14 20:57:48 +0000

    Improve print

--- FunctionalParadigm/Exercise-5/Program.fs
+++ FunctionalParadigm/Exercise-5/Program.fs
@@ -3,14 +3,14 @@
 [<EntryPoint>]
 let main _ =
     let ruta = solve con_paredes
-    printfn "Ruta corta con paredes: %A" ruta
+    printfn "Ruta corta con paredes: %A\n" ruta
 
     // Using a one-path function
     let ruta = solve_one_path sin_paredes
-    printfn "Primer camino sin paredes: %A" ruta
+    printfn "Primer camino sin paredes encontrado: %A" ruta
 
     // NOTE: This could take some minutes...
-    printfn "Calculando la mejor ruta sin paredes... (puede tardar unos minutos)"
+    printfn "Calculando la mejor ruta sin paredes de todas las posibles... (puede tardar unos minutos)"
     let ruta = solve sin_paredes
     printfn "Ruta corta sin paredes: %A" ruta
 
